c6ac871edee189b7f2ad234abe8597894965e9ca
jmpolak <jean_michel.polak@t-online.de>
2025-09-16 16:00:28 +0200

    Add global logger that logs on http request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { BasicController } from './interface/controllers/basic-controller';
 import { WalletModule } from './application/use-cases/wallet/wallet.module';
 import { FileModule } from './application/use-cases/file/file.module';
 import { DailyUpdateWalletCronJobModule } from './frameworks/cronjobs/daily-update-wallet-entry/daily-update-wallet.module';
+import { LoggerMiddleware } from './interface/middleware/http-request-logger';
 
 @Module({
   imports: [WalletModule, FileModule, DailyUpdateWalletCronJobModule],
   controllers: [BasicController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*'); // Applies to all routes
+  }
+}
