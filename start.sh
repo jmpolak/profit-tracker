caf03e161df60905c61dde15b933ddcf8b27fc8e
jmpolak <jean_michel.polak@t-online.de>
2025-09-18 11:42:48 +0200

    Create dev and prod files for docker

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Ensure a mode is passed
+if [ -z "$1" ]; then
+  echo -e "Error: No mode specified. Use 'dev' or 'prod'."
+  exit 1
+fi
+
+MODE=$1
+
+if [ "$MODE" == "dev" ]; then
+  echo -e "Starting in development mode..."
+  sudo docker-compose -f docker-compose.dev.yml up --build --watch
+
+elif [ "$MODE" == "prod" ]; then
+  echo -e "Starting in production mode..."
+  sudo docker-compose -f docker-compose.prod.yml up --build --watch
+
+else
+  echo -e "Invalid mode: $MODE. Use 'dev' or 'prod'."
+  exit 1
+fi
